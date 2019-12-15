1377d449453fad1fdc55e769da54123d2eb6220b
nathbenams <nathan_benamou@msn.com>
2019-12-15 04:06:33 +0200

    nathan-skeleton main

--- main.cpp
+++ main.cpp
@@ -7,8 +7,42 @@
 //
 
 #include <iostream>
+#include <pthread.h>
+#include <vector>
+using namespace std;
+#include "Bank.hpp"
+
+FILE* log;
+vector<account> listOfAccount;
 
 int main(int argc, const char * argv[]) {
     
+    listOfAccount.clear();
+    
+    log=fopen("log.txt","w");
+    if(log==nullptr){
+        perror("");
+        exit(1);
+    }
+    
+    pthread_t bankStatus,bankCommission;
+    //Create the fonction of bankstatus and bankcommission in bank.cpp
+    //and add pthread_create for them
+    
+    int numberOfCaspomat = atoi(argv[1]);
+    vector<*caspomat> listOfCaspomat;
+    vector<pthread_t> listOfThreadCaspomat;
+    
+    for(int i=0; i<numberOfCaspomat;i++){
+        // listofcaspomat.pushback(information of him and its file from argv)
+        //pthread_create with its function to execute
+    }
+    
+    for(int i=0;i<numberOfCaspomat;i++){
+        pthread_join(listOfThreadCaspomat.at(i), NULL);
+    }
+    
+    //pthread_join of the banks
+    fclose(log);
     return 0;
 }
