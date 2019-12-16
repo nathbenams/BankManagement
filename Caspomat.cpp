0c1042f5bccbc775dbfcf200be848494f3d7b1f9
nathbenams <nathan_benamou@msn.com>
2019-12-16 13:42:49 +0200

    nathan - mimouch execute for caspomat

--- Caspomat.cpp
+++ Caspomat.cpp
@@ -8,6 +8,8 @@
 
 #include "Caspomat.hpp"
 
+extern vector<Account> listOfAccount;
+
 Caspomat::Caspomat(int id,char* nameFile) : caspomatID(id),caspomatFile(nullptr),fileOpened(false)
 {
     caspomatFile= fopen(nameFile,"r");
@@ -22,5 +24,55 @@ Caspomat::~Caspomat()
 {
     if (fileOpened) {
         fclose(caspomatFile);
+    }
+}
+
+void Caspomat::executeCommandCaspomat()
+{
+    char cmd[MAX_LINE_SIZE];
+    char* command;
+    char* args[MAX_ARG];
+    const char* delimiters = " ";
+    int num_arg = 0;
+    
+    while(fgets(cmd, MAX_LINE_SIZE,caspomatFile) != nullptr){
+        command = strtok(cmd,delimiters);
+    
+        if(command==nullptr) break;
+        int accountID,accountPassword;
+    
+        for(int i = 0;i<MAX_ARG;i++){
+            args[i]=strtok(nullptr,delimiters);
+            if(args[i]!=nullptr){
+                num_arg++;
+            }
+        }
+        accountID = atoi(args[0]);
+        accountPassword = atoi(args[1]);
+        
+        if (!strcmp(command,"O")) {
+            //COMPLETE
+            continue;
+        }
+        else if(!strcmp(command,"D")){
+            //COMPLETE
+            continue;
+        }
+        else if(!strcmp(command,"W")){
+            //COMPLETE
+            continue;
+        }
+        else if(!strcmp(command,"B")){
+            //COMPLETE
+            continue;
+        }
+        else if(!strcmp(command,"T")){
+            //COMPLETE
+            continue;
+        }
+        else{
+            perror("Illegal command");
+            exit(1);
+        }
     }
 }
--- Caspomat.hpp
+++ Caspomat.hpp
@@ -11,12 +11,19 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+using namespace std;
+#include "Account.hpp"
+#define MAX_LINE_SIZE 50
+#define MAX_ARG 5
+
 
 class Caspomat
 {
 public:
     Caspomat(int id,char* nameFile);
     ~Caspomat();
+    void executeCommandCaspomat();
 private:
     int caspomatID;
     FILE* caspomatFile;
